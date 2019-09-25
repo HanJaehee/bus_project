fdab18f3b5c5fd6bc9f08cf681e1e48ae641c7f2
Ubuntu <ubuntu@ip-172-31-44-128.ap-northeast-2.compute.internal>
2019-09-25 20:17:41 +0900

    3개 정류장 추가

--- bus_ver_3/get_table_script.py
+++ bus_ver_3/get_table_script.py
@@ -1,7 +1,11 @@
 from table_create import * 
 
 tq = create_table("40115")
-tq.createbusinfo()
-tq.createbusremain()
+tq1 = create_table("08171")
+tq2 = create_table("11105")
+
+for i in [tq, tq1, tq2]:
+    i.createbusinfo()
+    i.createbusremain()
 
 #08-171, 40-115, 11-105
